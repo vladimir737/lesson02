0fc1592a0424e5d34c42a1870c4c510467c02f2b
Vladimir Kochetkov <kochetkov2209@yandex.ru>
2021-01-30 15:39:26 +0300

    lesson02

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+    let money = 50;
+    let income = 'фриланс';
+    let addExpenses = 'интернет, такси, коммуналка';
+    let deposit = true;
+    let mission = 200;
+    let period = 6;
+    alert("Первый урок");
+    console.log("Glo academy");
