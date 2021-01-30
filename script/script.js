a0072da966062ee8e8c089f842cf28214e139c2d
Vladimir Kochetkov <kochetkov2209@yandex.ru>
2021-01-30 20:15:43 +0300

    Правки ко 2 уроку

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
    //присвоение значений переменным
-    let money = 50;
+    let money = 50000;
     let income = 'фриланс';
     let addExpenses = 'Интернет, Такси, Коммуналка';
     let deposit = true;
-    let mission = 200;
+    let mission = 200000;
     let period = 6;
    //вывод в консоль типов данных 
     console.log(typeof money); 
@@ -13,10 +13,9 @@
     console.log(addExpenses.length); //вывод в консоль длину строки addExpenses
 
     console.log("Период равен", period, "месяцев");  
-    console.log("Цель заработать", mission, "тысяч рублей");
+    console.log("Цель заработать", mission, "рублей");
 
-    console.log(addExpenses.toLowerCase());  //приведение строки addExpenses к нижнему регистру и вывод в консоль
-    console.log(addExpenses.split(', '));   //вывод  массива в консоль  
-
+    console.log(addExpenses.toLowerCase().split(', '));  //приведение строки addExpenses к нижнему регистру
+    
     let budgetDay = money;  //присвоение переменной доход на месяц
-    console.log(budgetDay);  //вывод переменной в консоль
+    console.log(budgetDay/30);  //вывод переменной в консоль
